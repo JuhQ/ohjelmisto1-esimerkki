cdacb546bb90a127426afb7b5fa1a304f06b2adc
Juha Tauriainen <juha@bin.fi>
2024-08-20 14:42:41 +0300

    Tehtävä 2 esimerkkejä

---
+++ module2/module2_harjoite.py
@@ -0,0 +1,21 @@
+print("Hello")
+
+print('Hello 2')
+
+print('"Hei", sanoi Ville')
+print('"Hei", sanoi Ville')
+
+print("Hyvää huomenta")
+print("Hyvää")
+print("huomenta")
+
+print("Hyvää\nhuomenta")
+
+print("Juha\nTauriainen")
+
+
+nimi = input("Anna nimi:")
+print(nimi)
+
+nimi2 = "Juha"
+print(nimi2)
