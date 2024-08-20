a7287dd0b823c7fa69d559921e08812992935ece
Juha Tauriainen <juha@bin.fi>
2024-08-20 15:51:19 +0300

    Lisää tehtävä 2 esimerkkejä

---
+++ module2/formatointi.py
@@ -0,0 +1,19 @@
+arvo = 123.12344567
+nimi = "Juha"
+
+print(arvo)
+
+laskun_tulos = arvo + arvo
+muuttuja1 = 2.4 ** 4
+
+print(muuttuja1)
+print(f"{muuttuja1: .2f}")
+
+print(f"Arvo: {arvo}")
+print(f"Arvo: {arvo: 2.2f}")
+print(f"Arvo: {arvo: 2.5f}")
+print(f"Arvo: {arvo: <2.5f}")
+print(f"Arvo: {arvo: .5f}")
+print(f"Arvo: {arvo: <.5f}, nimi = {nimi}")
+print(f"Plus lasku: {arvo} + {arvo} = {arvo + arvo}")
+print(f"Laskun tulos: {laskun_tulos}")
